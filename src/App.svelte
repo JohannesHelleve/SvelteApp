93849d08ab58290dbaf7b30fd5fb6720a9b3b8f4
Johannes Helleve <Johanshelleve@gmail.com>
2023-06-23 00:22:48 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+	let name = "Sjefen";
+	let color = "black";
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>{color} blet</p>
+	<button on:click={() => {color = 'orange'}}>Update color</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
